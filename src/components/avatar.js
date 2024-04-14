9cf2e83e6e651d8e97b7ee3bca0720b46160d80a
Ilya Pozvolenko <BinaryCompound2@gmail.com>
2024-04-14 21:17:01 +0300

    editimg

---
+++ src/components/avatar.js
@@ -0,0 +1,35 @@
+import { openModal, closeModal } from './modal.js';
+import { updateAvatar } from '../index.js';
+
+// Функция для обработки отправки формы обновления аватара
+export function handleAvatarFormSubmit(evt) {
+  evt.preventDefault();
+  const avatarUrl = document.getElementById('avatar__input').value;
+  updateAvatar(avatarUrl)
+    .then(() => {
+      // Если запрос успешен, обновляем аватар на странице и закрываем модальное окно
+      updateAvatarOnPage(avatarUrl);
+      closeAvatarModal();
+    })
+    .catch((err) => {
+      console.error('Ошибка при обновлении аватара:', err);
+    });
+}
+
+// Функция для обновления аватара на странице
+function updateAvatarOnPage(avatarUrl) {
+  const profileImage = document.querySelector('.profile__image');
+  profileImage.src = avatarUrl;
+}
+
+// Функция для открытия модального окна обновления аватара
+export function openAvatarModal() {
+  const modalWindow = document.querySelector('.popup_type_new-avatar');
+  openModal(modalWindow);
+}
+
+// Функция для закрытия модального окна обновления аватара
+export function closeAvatarModal() {
+  const modalWindow = document.querySelector('.popup_type_new-avatar');
+  closeModal(modalWindow);
+}
